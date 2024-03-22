fbcff832c59648fc8c1f3adcb3abfff3d52196c2
sunalipatro <sunalipatro6@gmail.com>
2024-03-23 00:17:32 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ def home():
 
 
 @app.route('/predict_api', methods=['POST'])
-
 def predict_api():
     data = request.json['data'] # we are getting the data in json format.
     print(data)
@@ -24,6 +23,16 @@ def predict_api():
     output = regmodel.predict(data)
     print(output[0])   ## output is a 2d array.
     return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=np.array(data).reshape(1,-1)
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 
 
 if __name__ == "__main__":
